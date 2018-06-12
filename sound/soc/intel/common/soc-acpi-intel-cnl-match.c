c87e83cbe4f50dafdb6e84b9bf962348fa1ac74b
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2018-06-12 18:52:58 -0500

    ASoC: Intel: common: add ACPI match for CannonLake
    MX98373 is used on some CNL boards
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- sound/soc/intel/common/soc-acpi-intel-cnl-match.c
+++ sound/soc/intel/common/soc-acpi-intel-cnl-match.c
@@ -25,6 +25,14 @@ struct snd_soc_acpi_mach snd_soc_acpi_intel_cnl_machines[] = {
 		.asoc_plat_name = "0000:00:1f.3",
 	},
 	{
+		.id = "MX98373",
+		.drv_name = "cnl_max98373",
+		.fw_filename = "intel/dsp_fw_cnl.bin",
+		.pdata = &cnl_pdata,
+		.sof_fw_filename = "sof-cnl.ri",
+		.sof_tplg_filename = "sof-cnl.tplg",
+	},
+	{
 		.id = "10EC5682",
 		.drv_name = "sof_rt5682",
 		.sof_fw_filename = "sof-cnl.ri",
