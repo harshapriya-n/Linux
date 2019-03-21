6bd4c171f02cd04c480a81676f88a7a30cff1bbc
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2019-03-21 11:51:26 -0700

    Revert "ASoC: SOF: remove warnings with min() and min3()"
    This reverts commit c283acdb280eb8c85609494c104e3fe176ee1810.

--- sound/soc/sof/debug.c
+++ sound/soc/sof/debug.c
@@ -41,9 +41,7 @@ static ssize_t sof_dfsentry_read(struct file *file, char __user *buffer,
 		return -EINVAL;
 	if (pos >= size || !count)
 		return 0;
-	/* find the minimum. min() is not used since it adds sparse warnings */
-	if (count > size - pos)
-		count = size - pos;
+	count = min(count, (size_t)(size - pos));
 
 	/* intermediate buffer size must be u32 multiple */
 	size = round_up(count, 4);
