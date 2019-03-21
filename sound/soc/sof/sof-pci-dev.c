d103bfae5744a9cb085411f05d00cf3d8dbe6239
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2019-03-21 11:44:41 -0700

    Revert "ASoC: SOF: remove use of asoc_plat_name"
    This reverts commit afe2a90ce7ddb1f42efff4d7449aef93b3d98270.

--- sound/soc/sof/sof-pci-dev.c
+++ sound/soc/sof/sof-pci-dev.c
@@ -72,6 +72,7 @@ static struct snd_soc_acpi_mach sof_tng_machines[] = {
 		.drv_name = "edison",
 		.sof_fw_filename = "sof-byt.ri",
 		.sof_tplg_filename = "sof-byt.tplg",
+		.asoc_plat_name = "baytrail-pcm-audio",
 	},
 	{}
 };
