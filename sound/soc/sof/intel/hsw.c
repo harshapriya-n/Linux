8ca687e5f1b8ab484e99a9398838b88226dcfd71
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2019-03-21 11:44:41 -0700

    Revert "ASoC: SOF: loader: remove cl_bar field"
    This reverts commit 2639d1a67bf4da8de3891073f0f6860038a8ee93.

--- sound/soc/sof/intel/hsw.c
+++ sound/soc/sof/intel/hsw.c
@@ -618,6 +618,9 @@ static int hsw_probe(struct snd_sof_dev *sdev)
 		return ret;
 	}
 
+	/* set BARS */
+	sdev->cl_bar = HSW_DSP_BAR;
+
 	/* set default mailbox */
 	snd_sof_dsp_mailbox_init(sdev, MBOX_OFFSET, MBOX_SIZE, 0, 0);
 
