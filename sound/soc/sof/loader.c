fc24650df15a6c112e91fa0340e0a44075604dd9
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2019-03-21 11:44:41 -0700

    Revert "ASoC: SOF: Allow block ops from multiple bars"
    This reverts commit e0d298e6a06fd6a88603fb1c50a16ef41909cd0e.

--- sound/soc/sof/loader.c
+++ sound/soc/sof/loader.c
@@ -45,14 +45,13 @@ int snd_sof_fw_parse_ext_data(struct snd_sof_dev *sdev, u32 offset)
 		return -ENOMEM;
 
 	/* get first header */
-	snd_sof_dsp_block_read(sdev, sdev->mmio_bar, offset, ext_data,
-			       sizeof(*ext_hdr));
+	snd_sof_dsp_block_read(sdev, offset, ext_data, sizeof(*ext_hdr));
 	ext_hdr = (struct sof_ipc_ext_data_hdr *)ext_data;
 
 	while (ext_hdr->hdr.cmd == SOF_IPC_FW_READY) {
 		/* read in ext structure */
 		offset += sizeof(*ext_hdr);
-		snd_sof_dsp_block_read(sdev, sdev->mmio_bar, offset,
+		snd_sof_dsp_block_read(sdev, offset,
 				       ext_data + sizeof(*ext_hdr),
 				       ext_hdr->hdr.size - sizeof(*ext_hdr));
 
@@ -78,7 +77,7 @@ int snd_sof_fw_parse_ext_data(struct snd_sof_dev *sdev, u32 offset)
 
 		/* move to next header */
 		offset += ext_hdr->hdr.size;
-		snd_sof_dsp_block_read(sdev, sdev->mmio_bar, offset, ext_data,
+		snd_sof_dsp_block_read(sdev, offset, ext_data,
 				       sizeof(*ext_hdr));
 		ext_hdr = (struct sof_ipc_ext_data_hdr *)ext_data;
 	}
@@ -147,7 +146,7 @@ int snd_sof_parse_module_memcpy(struct snd_sof_dev *sdev,
 				block->size);
 			return -EINVAL;
 		}
-		snd_sof_dsp_block_write(sdev, sdev->mmio_bar, offset,
+		snd_sof_dsp_block_write(sdev, offset,
 					(void *)block + sizeof(*block),
 					block->size);
 
