6ea26ff1c4c3c6aa48c7213a4fe101478b2e85c5
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2019-03-21 11:51:05 -0700

    Revert "ASoC: SOF: remove redundant ops table on spi platform"
    This reverts commit 25eaf2cd7bc1d5bda5013745a90174f6292e64ef.

--- sound/soc/sof/sof-spi-dev.c
+++ sound/soc/sof/sof-spi-dev.c
@@ -18,7 +18,6 @@
 #include <linux/spi/spi.h>
 #include <sound/pcm.h>
 #include <sound/sof.h>
-#include "intel/shim.h"
 #include "sof-priv.h"
 #include "hw-spi.h"
 #include "ops.h"
@@ -43,7 +42,10 @@ static const struct sof_dev_desc spi_desc = {
 	.resindex_imr_base = -1,
 	.irqindex_host_ipc = -1,
 	.resindex_dma_base = -1,
-	.chip_info = &spi_chip_info,
+};
+
+static const struct sof_ops_table spi_mach_ops[] = {
+	{&spi_desc, &snd_sof_spi_ops},
 };
 
 static int sof_spi_probe(struct spi_device *spi)
@@ -51,7 +53,6 @@ static int sof_spi_probe(struct spi_device *spi)
 	struct device *dev = &spi->dev;
 	const struct sof_dev_desc *desc = of_device_get_match_data(dev);
 	struct snd_soc_acpi_mach *machines, *mach;
-	const struct sof_intel_dsp_desc *chip_info;
 	struct snd_sof_pdata *sof_pdata;
 	struct sof_platform_priv *priv;
 	const char *tplg, *fw;
@@ -122,8 +123,9 @@ static int sof_spi_probe(struct spi_device *spi)
 	 * to add a dedicated ops field in the generic soc-acpi structure
 	 * to avoid such issues
 	 */
-	chip_info = (const struct sof_intel_dsp_desc *)desc->chip_info;
-	mach->pdata = (void *)chip_info->ops;
+
+	mach->pdata = (void *)sof_get_ops(desc, spi_mach_ops,
+					  ARRAY_SIZE(spi_mach_ops));
 	if (!mach->pdata) {
 		dev_err(dev, "error: no matching SPI descriptor ops\n");
 		return -ENODEV;
