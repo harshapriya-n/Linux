37880bc0fdcf86ddc627ca8361c13369689f99bc
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2019-03-21 11:44:41 -0700

    Revert "ASoC: SOF: Intel: cnl: remove unneeded inits"
    This reverts commit c99c3e14e427ec6e25ad7a2acec5dd5f7b7d5255.

--- sound/soc/sof/intel/cnl.c
+++ sound/soc/sof/intel/cnl.c
@@ -29,13 +29,7 @@ static int cnl_ipc_cmd_done(struct snd_sof_dev *sdev, int dir);
 static irqreturn_t cnl_ipc_irq_thread(int irq, void *context)
 {
 	struct snd_sof_dev *sdev = (struct snd_sof_dev *)context;
-	u32 hipci;
-	u32 hipcctl;
-	u32 hipcida;
-	u32 hipctdr;
-	u32 hipctdd;
-	u32 msg;
-	u32 msg_ext;
+	u32 hipci, hipcctl, hipcida, hipctdr, hipctdd, msg = 0, msg_ext = 0;
 	irqreturn_t ret = IRQ_NONE;
 
 	/* here we handle IPC interrupts only */
