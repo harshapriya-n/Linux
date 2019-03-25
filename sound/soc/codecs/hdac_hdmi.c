87302e6cf04ba3ad33d95783a118b2af12da8c0c
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2019-03-24 18:43:37 -0700

    Revert "[BACKPORT] ALSA: hda: Refactor display power management"
    This reverts commit a2f3ba8be8ec33eddf2618b8381faf9b32f59165.

--- sound/soc/codecs/hdac_hdmi.c
+++ sound/soc/codecs/hdac_hdmi.c
@@ -2054,7 +2054,13 @@ static int hdac_hdmi_dev_probe(struct hdac_device *hdev)
 	 * Turned off in the runtime_suspend during the first explicit
 	 * pm_runtime_suspend call.
 	 */
-	snd_hdac_display_power(hdev->bus, hdev->addr, true);
+	ret = snd_hdac_display_power(hdev->bus, true);
+	if (ret < 0) {
+		dev_err(&hdev->dev,
+			"Cannot turn on display power on i915 err: %d\n",
+			ret);
+		return ret;
+	}
 
 	ret = hdac_hdmi_parse_and_map_nid(hdev, &hdmi_dais, &num_dais);
 	if (ret < 0) {
@@ -2145,7 +2151,9 @@ static int hdac_hdmi_runtime_suspend(struct device *dev)
 
 	snd_hdac_ext_bus_link_put(bus, hlink);
 
-	snd_hdac_display_power(bus, hdev->addr, false);
+	err = snd_hdac_display_power(bus, false);
+	if (err < 0)
+		dev_err(dev, "Cannot turn off display power on i915\n");
 
 	return 0;
 }
@@ -2170,7 +2178,11 @@ static int hdac_hdmi_runtime_resume(struct device *dev)
 
 	snd_hdac_ext_bus_link_get(bus, hlink);
 
-	snd_hdac_display_power(bus, hdev->addr, true);
+	err = snd_hdac_display_power(bus, true);
+	if (err < 0) {
+		dev_err(dev, "Cannot turn on display power on i915\n");
+		return err;
+	}
 
 	hdac_hdmi_skl_enable_all_pins(hdev);
 	hdac_hdmi_skl_enable_dp12(hdev);
