6b42937ebf77ee3c118c92c5dbe993629bc3f73b
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2019-11-26 15:09:27 -0800

    [DEBUG ONLY] ASoC: SOF: ipc: remove error when no reply is expected
    Change-Id: I801700e71c73e656131a95088a089acba094aef4
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>

--- sound/soc/sof/ipc.c
+++ sound/soc/sof/ipc.c
@@ -320,8 +320,6 @@ int snd_sof_ipc_reply(struct snd_sof_dev *sdev, u32 msg_id)
 	struct snd_sof_ipc_msg *msg = &sdev->ipc->msg;
 
 	if (msg->ipc_complete) {
-		dev_err(sdev->dev, "error: no reply expected, received 0x%x",
-			msg_id);
 		return -EINVAL;
 	}
 
